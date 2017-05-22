1b2ec446eb7118049edbb95a8eab01fc30f00cac
Špela Ognjanović <spela.og@gmail.com>
2017-05-22 17:55:46 +0200

    Dokončaj gumbe

--- Glavni_program.py
+++ Glavni_program.py
@@ -5,7 +5,7 @@ slovenska_abeceda = ['A', 'B', 'C', 'Č', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K'
 
 okno = tk.Tk()
 okno.resizable(width=False, height=False)#uporabnik ne more spreminjati velikosti okna
-okno.geometry('{}x{}'.format(700, 500)) 
+okno.geometry('{}x{}'.format(800, 600)) 
 
 
 #Funkcija,ki bo postavila gumbe
@@ -24,10 +24,10 @@ def nova_igra():
     gumb_nov_zacetek = tk.Button(okno, text='Nova igra')
     gumb_nov_zacetek.place(relx=0.73, rely=0.3)
 
-
+poteza = 0
 
 #Funkcija, ki nariše vislice
-slika = tk.Canvas(okno,width=400,height=500)
+slika = tk.Canvas(okno,width=500,height=800)
 def vislice(n): #n bo najvec 10, torej 10 možnosti za ugib črk, drugače se izrišejo vislice
     if n == 1:
         slika.create_polygon(50,305,50,310,150,310,150,305)
@@ -38,27 +38,77 @@ def vislice(n): #n bo najvec 10, torej 10 možnosti za ugib črk, drugače se iz
     if n == 4:
         slika.create_polygon(5,175,5,200,10,200,10,175)
     if n == 5:
-        slika.create_oval(2,200,25,225)#vneseš 1. in 3. koordinato
-
-
+        slika.create_oval(2,200,22,220)#vneseš 1. in 3. koordinato
+    if n == 6:
+        slika.create_polygon(11,220,11,270,13,270,13,220)
+    if n == 7:
+        slika.create_polygon(0,200,0,202,11,237,11,235)
+    if n == 8:
+        slika.create_polygon(13,235,13,237,28,202,28,200)
+    if n == 9:
+        slika.create_polygon(0,300,0,302,11,270,11,268)
+    if n == 10:
+        slika.create_polygon(13,268,13,270,24,302,24,300)
     
 slika.pack()
 
-#Funkcija, ki presteje stevilo crk v besedi
-#def st_crk(beseda): #lahko še narandom izberemo besedo
-    #st_crk = beseda.count
-
 #Funkcija, ki nariše št. črt
-#def narici_crte(st_crk):
+def narici_crte(beseda):
+    st_crk = len(beseda)
+    x = -15
+    for i in range(st_crk):
+        x+=35
+        slika.create_polygon(x,515,x,517,x+20,517,x+20,515)
+
+
+# Funkcijo. ki vzame kot argument besedo in crko
+#in nato izpiše/izriše črke na črto
+def izpisi_crko(beseda, crka):
+    global poteza #Poeni, da spremenljivka živi
+    #zunaj funkcije in ta funkcija jo uporablja
+    st_uganjenih =0
+    for i in range(len(beseda)):       
+        if beseda[i] == crka:
+            st_uganjenih +=1
+            slika.create_text(30 + i*35, 505, text=crka.upper())
+    if st_uganjenih == 0:
+        poteza +=1      
+        vislice(poteza)
+        
+class Gumb:
+    def  __init__(self,i ,j, okvir, beseda ):#i,j sta koordinati od 1-500
+        self.gumb = tk.Button(okvir, text=slovenska_abeceda[5*i + j], command = self.izpisi)
+        self.beseda = beseda
+        self.i = i #vsak gumb se more zavedat na kateri koordinati je
+        self.j= j
+
+    def izpisi(self):
+        izpisi_crko(self.beseda,slovenska_abeceda[5*self.i + self.j])#crka je ko klikneš
+        #se ko pritisneš da gumb ostane noter stisnjenS
+        
+        
+
+#gumbi za abecedo          
+class Gumbi:
+    def __init__:
+        
+        
     
+    
+    
+
+
 
 postavi_abecedo() #še pokliči funkcijo
 nova_igra()
-vislice(1)
-vislice(2)
-vislice(3)
-vislice(4)
-vislice(5)
+
+narici_crte('avtomobil')
+izpisi_crko('avtomobil', 'a')
+izpisi_crko('avtomobil', 'o')
+izpisi_crko('avtomobil', 'š')
+izpisi_crko('avtomobil', 'đ')
+
+
 tk.mainloop()
     
 
